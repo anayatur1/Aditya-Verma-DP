92b6eca2ff773c829701f1351255356780bb3616
anayatur1 <119777828+anayatur1@users.noreply.github.com>
2025-04-16 23:38:57 +0530

    Update 34_Palindrome_partitioning_memoization_optimization.cpp
    Updated code it was giving TLE earlier.

--- 34_Palindrome_partitioning_memoization_optimization.cpp
+++ 34_Palindrome_partitioning_memoization_optimization.cpp
@@ -42,3 +42,64 @@ public:
         return Solve(s, 0, n, t) - 1;
     }
 };
+
+//Added By rahman
+//LeetCode solution passing all test cases. When we get a palindrome, we take it as one cut and then recursively solve the rest. 
+// Recursion calls are reduced this way. Also, string should be passed as reference both in isPalindrome and solve functions, otherwise would give TLE.
+
+class Solution {
+public:
+ int t[2001][2001];
+ 	bool isPalindrome(string &s, int i, int j)
+ 	{
+ 		while (i < j)
+ 		{
+ 			if (s[i] != s[j])
+ 			{
+ 				return false;
+ 			}
+
+ 			i++;
+ 			j--;
+ 		}
+
+ 		return true;
+ 	}
+
+ 	int solve(string &s, int i, int j)
+ 	{
+ 		if (i >= j)
+ 		{
+ 			return 0;
+ 		}
+
+ 		if (t[i][j] != -1)
+ 		{
+ 			return t[i][j];
+ 		}
+
+ 		if (isPalindrome(s, i, j) == true)
+ 		{
+ 			return 0;
+ 		}
+
+ 		int ans = INT_MAX;
+ 		for (int k = i; k<=j-1; k++)
+ 		{
+ 			if (isPalindrome(s, i, k))
+ 			{
+ 				ans = min(ans, 1 + solve(s, k + 1, j));
+ 			}
+ 		}
+
+ 		return t[i][j] = ans;
+ 	}
+
+ 	int minCut(string s)
+ 	{
+ 		int i = 0;
+ 		int j = s.length() - 1;
+ 		memset(t, -1, sizeof(t));
+ 		return solve(s, i, j);
+ 	}
+};
